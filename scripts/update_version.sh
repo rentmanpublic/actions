871cd9933ef15efb3640dbba203c1128e77e4eaa
Douwe Schepers <douwe.schepers@rentman.nl>
2025-01-15 14:07:25 +0100

    add npm version update to update the package.json

--- scripts/update_version.sh
+++ scripts/update_version.sh
@@ -6,45 +6,46 @@ if [[ -z "$GIT_TAG" ]]; then
     exit 1;
 fi
 
-# Add .php extension to the filename
-file="$TARGET_REPOSITORY_FOLDER/$VERSION_FILE_PATH"
-
-echo 'Starting'
-if [ -f $file ]; then
-  chmod 700 $file
-else
-  echo "File does not exist:"
-  ls -l
-fi
-
 if [ $PROGRAMMING_LANGUAGE == "php" ]; then
+  # Add .php extension to the filename
+  file="$TARGET_REPOSITORY_FOLDER/$VERSION_FILE_PATH"
+
+  echo 'Starting'
+  if [ -f $file ]; then
+    chmod 700 $file
+  else
+    echo "File does not exist:"
+    ls -l
+  fi
+
   # Write content to the file
   echo "<?php" > "$file"
   echo "" >> "$file"
   echo "namespace Rentman;" >> "$file"
   echo "" >> "$file"
   echo "const RM_VERSION = '$GIT_TAG';"  >> "$file"
-
-elif [ $PROGRAMMING_LANGUAGE == "typescript" ]; then
-  # Write content to the file
-  echo "const RM_VERSION = '$GIT_TAG';"  >> "$file"
-fi
-
-# Make sure the file is updated and display success message
-if grep -q "$GIT_TAG" "$file"; then
+  # Make sure the file is updated and display success message
+  if grep -q "$GIT_TAG" "$file"; then
 
     cd ./$TARGET_REPOSITORY_FOLDER
     git config --global user.email "buildbot@rentman.nl"
     git config --global user.name "BuildBot"
-
     git add $VERSION_FILE_PATH
     git commit -m "Bump version to $GIT_TAG"
     git tag $GIT_TAG
     git push
     git push origin tag $GIT_TAG
 
-    echo "Version file '$GIT_TAG' updated successfully!"
-else
-    echo "Failed to create the version file."
-    exit 1;
+      echo "Version file '$GIT_TAG' updated successfully!"
+  else
+      echo "Failed to create the version file."
+      exit 1;
+  fi
+fi
+
+if [ $PROGRAMMING_LANGUAGE == "typescript" ]; then
+  # Write content to the file
+  cd ./$TARGET_REPOSITORY_FOLDER
+  npm version minor
+  git push
 fi
