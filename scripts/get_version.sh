3af4b3c38f8d2eb674648b76bbee06aaa96ae5f9
Nan Damhuis <nan@rentman.nl>
2025-01-17 15:19:33 +0100

    Fix version output parsing

--- scripts/get_version.sh
+++ scripts/get_version.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 
-grep --version
-
 if [ $PROGRAMMING_LANGUAGE == "php" ]; then
   file="$TARGET_REPOSITORY_FOLDER/$VERSION_FILE_PATH"
   version=$(grep -o "const RM_VERSION = '[^']*'" $file | sed "s/const RM_VERSION = '\([^']*\)'/\1/")
   echo "Found version: $version"
-  echo "version=$version" >> $GITHUB_OUTPUT
+  echo "get_version=$version" >> $GITHUB_OUTPUT
+
+  exit 0
 fi
 
 if [ $PROGRAMMING_LANGUAGE == "typescript" ]; then
   file="$TARGET_REPOSITORY_FOLDER/package.json"
   version=npm version --json | jq -r '.package'
-  echo "version=" >> $GITHUB_OUTPUT
+  echo "get_version=$version" >> $GITHUB_OUTPUT
+
+  exit 0
 fi
