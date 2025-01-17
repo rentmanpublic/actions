df829ff69e597d09a825b4f960f123f0bcac068b
Nan Damhuis <nan@rentman.nl>
2025-01-17 15:43:20 +0100

    Remove reserved keyword

--- scripts/get_version.sh
+++ scripts/get_version.sh
@@ -4,11 +4,11 @@ if [ $PROGRAMMING_LANGUAGE == "php" ]; then
   file="$TARGET_REPOSITORY_FOLDER/$VERSION_FILE_PATH"
   version=$(grep -o "const RM_VERSION = '[^']*'" $file | sed "s/const RM_VERSION = '\([^']*\)'/\1/")
   echo "Found version: $version"
-  echo "version=$version" >> $GITHUB_OUTPUT
+  echo "determined-version=$version" >> $GITHUB_OUTPUT
 fi
 
 if [ $PROGRAMMING_LANGUAGE == "typescript" ]; then
   file="$TARGET_REPOSITORY_FOLDER/package.json"
   version=npm version --json | jq -r '.package'
-  echo "version=$version" >> $GITHUB_OUTPUT
+  echo "determined-version=$version" >> $GITHUB_OUTPUT
 fi
