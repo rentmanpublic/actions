f3f209b15e43a83d1c6fc4c51b942fb27647510b
Douwe Schepers <douwe.schepers@rentman.nl>
2025-01-30 14:00:30 +0100

    find service name based on tag if the tag is given

--- scripts/get_service_name_by_tag.sh
+++ scripts/get_service_name_by_tag.sh
@@ -1,49 +1,29 @@
 #!/usr/bin/env bash
 
-if [[ -z "$SERVICE_NAME" ]]; then
-    echo "No environment provided"
+if [[ -z "$SERVICE_NAME" && -z "$SERVICE_TAG" ]]; then
+    echo "No service name or service tag provided. At least one of the two should be given"
     exit 1;
 fi
 
-SERVICE_NAME_LOWER=$(echo "$SERVICE_NAME" | tr '[:upper:]' '[:lower:]')
+if [[ -z "$SERVICE_TAG" && "$SERVICE_NAME" ]]; then
+    # just return the name of the service
+    echo "No service name provided"
+    echo "service_name=$SERVICE_NAME"
+    echo "service_name=$SERVICE_NAME" >> "$GITHUB_ENV"
+    exit 1;
+else
+  SERVICE_NAME_FOUND=$(aws resourcegroupstaggingapi get-resources \
+    --region "$REGION" \
+    --resource-type-filters ecs:service \
+    --tag-filters Key=Service,Values="$TAG_NAME" \
+    --query "ResourceTagMappingList[*].ResourceARN" \
+    --output text | grep "/$CLUSTER_NAME/" | awk -F'/' '{print $NF}')
 
-# Map the values of the service name in github actions to the tag names in the ECS cluster
-case "$SERVICE_NAME_LOWER" in
-    "tools")
-        TAG_NAME="toolsendpoint"
-        ;;
-    "slack")
-        TAG_NAME="rentman-slack"
-        ;;
-    "adminserver")
-        TAG_NAME="adminserver"
-        ;;
-    "worker-scheduler")
-        TAG_NAME="worker-scheduler"
-        ;;
-    "versiondiscovery")
-        TAG_NAME="versiondiscovery"
-        ;;
-    "rentman-translations")
-        TAG_NAME="translations"
-        ;;
-    *)
-        echo "No tag defined for tag value: $SERVICE_NAME"
+    # Handle case when no output is returned
+    if [[ -z "$SERVICE_NAME_FOUND" ]]; then
+        echo "No service found with the tag: $SERVICE_TAG"
         exit 1
-        ;;
-esac
-
-OUTPUT=$(aws resourcegroupstaggingapi get-resources \
-  --region "$REGION" \
-  --resource-type-filters ecs:service \
-  --tag-filters Key=Service,Values="$TAG_NAME" \
-  --query "ResourceTagMappingList[*].ResourceARN" \
-  --output text | grep "/$CLUSTER_NAME/" | awk -F'/' '{print $NF}')
-
-# Handle case when no output is returned
-if [[ -z "$OUTPUT" ]]; then
-    echo "No clusters found with the tag: ClusterName=$OUTPUT"
-    exit 1
+    fi
+    echo "service_name=$SERVICE_NAME_FOUND"
+    echo "service_name=$SERVICE_NAME_FOUND" >> "$GITHUB_ENV"
 fi
-echo "service_name=$OUTPUT"
-echo "service_name=$OUTPUT" >> "$GITHUB_ENV"
