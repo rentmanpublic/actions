0e71fe1197fec35ee3763b2abcb48f2763261dc7
Nan Damhuis <nan@rentman.nl>
2025-01-31 11:38:12 +0100

    Add more logging to tag fetching

--- scripts/get_service_name_by_tag.sh
+++ scripts/get_service_name_by_tag.sh
@@ -7,18 +7,20 @@ fi
 
 if [[ -z "$SERVICE_TAG" && "$SERVICE_NAME" ]]; then
     # just return the name of the service
-    echo "No service name provided"
+    echo "No service tag provided, using name"
     echo "service_name=$SERVICE_NAME"
     echo "service_name=$SERVICE_NAME" >> "$GITHUB_OUTPUT"
     exit 0
 else
+  echo "Getting service tag $SERVICE_TAG"
   SERVICE_NAME_FOUND=$(aws resourcegroupstaggingapi get-resources \
     --region "$REGION" \
     --resource-type-filters ecs:service \
-    --tag-filters Key=Service,Values="$SERVICE_TAG" \
+    --tag-filters Key=Service,Values=$SERVICE_TAG \
     --query "ResourceTagMappingList[*].ResourceARN" \
     --output text | grep "/$CLUSTER_NAME/" | awk -F'/' '{print $NF}')
 
+echo "Output of aws request: $SERVICE_NAME_FOUND"
     # Handle case when no output is returned
     if [[ -z "$SERVICE_NAME_FOUND" ]]; then
         echo "No service found with the tag: $SERVICE_TAG"
